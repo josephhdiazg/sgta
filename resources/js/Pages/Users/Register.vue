ef040387fc2829faccfd61a12e76fb5404dd4287
josephhdiazg <josephhansdiaz30@gmail.com>
2025-05-30 11:09:50 -0500

    fix(frontend): :bug: Translate Register.vue

--- resources/js/Pages/Users/Register.vue
+++ resources/js/Pages/Users/Register.vue
@@ -1,9 +1,8 @@
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, useForm } from '@inertiajs/vue3';
 import FormError from '@/Components/FormError.vue';
 
 const form = useForm({
-    handle: '',
     name: '',
     email: '',
     password: '',
@@ -22,38 +21,33 @@ const form = useForm({
 
                 <form @submit.prevent="form.post(route('users.store'))" class="max-w-sm mx-auto">
                     <div class="mb-5">
-                        <label for="handle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your username or handle</label>
-                        <FormError :error="form.errors?.handle" />
-                        <input v-model="form.handle" type="text" id="handle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-                    </div>
-                    <div class="mb-5">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your display name</label>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre completo</label>
                         <FormError :error="form.errors?.name" />
                         <input v-model="form.name" type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
                     <div class="mb-5">
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
                         <FormError :error="form.errors?.email" />
                         <input v-model="form.email" type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@domain.com" required />
                     </div>
                     <div class="mb-5">
-                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Contraseña</label>
                         <FormError :error="form.errors?.password" />
                         <input v-model="form.password" type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
                     <div class="mb-5">
-                        <label for="password-confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
+                        <label for="password-confirmation" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirme su contraseña</label>
                         <FormError :error="form.errors?.password_confirmation" />
                         <input v-model="form.password_confirmation" type="password" id="password-confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                     </div>
 
                     <div class="flex items-start mb-8">
-                        <span class="text-sm italic font-medium text-gray-900 dark:text-gray-300">By clicking "Create account", you confirm that you agree with Sushiroll's <a href="#" target="_blank" class="text-blue-600 hover:underline dark:text-blue-500">Terms and Conditions of Service</a>. <!-- , and are acknowledging our Privacy Policy applies. --> </span>
+                        <span class="text-sm italic font-medium text-gray-900 dark:text-gray-300">Haciendo click en "Crear cuenta", usted acepta nuestros Terminos y Condiciones de Servicio.</span>
                     </div>
 
-                    <Link :href="route('login')" class="my-2.5 float-right text-blue-600 text-sm hover:underline dark:text-blue-500">I already have an account</Link>
+                    <Link :href="route('login')" class="my-2.5 float-right text-blue-600 text-sm hover:underline dark:text-blue-500">Ya tengo una cuenta</Link>
 
-                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create account</button>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crear cuenta</button>
                 </form>
             </div>
         </div>
